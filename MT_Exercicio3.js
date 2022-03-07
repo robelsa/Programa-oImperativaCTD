93e504ef48c4e04271d3180262a68e3d0e6d629d
Robelsa Vasconcelos <robelsapl>
2022-03-07 20:06:01 -0300

    Alterei o nome do arquivo

---
+++ MT_Exercicio3.js
@@ -0,0 +1,40 @@
+// Equipe: Robelsa Vasconcelos; 
+
+//1. Exiba a soma de 2 números
+let x=10;
+let y=5;
+console.log(x+=y);
+
+//2. Repita o anterior, mas utilizando outro operador
+console.log(x*=y);
+
+//3. Declare e atribua 2 variáveis independentes. Exiba a soma de ambas na tela. 
+
+var numero1=4;
+var numero2=8;
+var soma=numero1+numero2;
+console.log(soma);
+
+//4. Atribua a soma das duas variáveis anteriores a uma outra variável e exiba-a na tela.
+
+var numero3=10;
+console.log(soma+numero3);
+
+//5. Re-atribua "Olá" + 23 e exiba na tela.
+
+let saudacao="Olá!";
+let num=23;
+console.log(saudacao+num);
+
+// Crie mensagens concatenadas com valores diferentes. 
+
+var nome="Robelsa";
+var idade= "37 anos";
+var cidade= "Pedra Lavrada";
+var estado="Paraíba";
+console.log(nome+idade+cidade+estado);
+
+
+//Como separar as informações? 
+
+
