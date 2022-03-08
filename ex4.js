240fda51e6e9f8ae62ec3eba19a6fe96e6cd0675
Robelsa Vasconcelos <robelsapl>
2022-03-08 19:50:32 -0300

    Atividades para calcular o IMC

---
+++ ex4.js
@@ -0,0 +1,44 @@
+//Sala 2 : Robelsa, Felipe Albarello, Davi Dutra, Cadu Menezes, Sara Marques.
+
+let nome1="José";
+let nome2="Carlos";
+let nome3="Aline";
+let nome4= "Ana";
+
+let sobrenome1= " da Silva";
+let sobrenome2="de Souza";
+let sobrenome3="Ferreira";
+let sobrenome4="Paula";
+
+let idade1=27;
+let idade2=28;
+let idade3=33;
+let idade4=26;
+
+let peso1=83.5;
+let peso2=80.1;
+let peso3=63.7;
+let peso4=55.0;
+
+let altura1=1.70;
+let altura2=1.76;
+let altura3=1.53;
+let altura4=1.62;
+
+let plano1="Ouro";
+let plano2="Diamante";
+let plano3="Não possui =(";
+let plano4="Prata";
+
+let IMC1=(peso1/altura1**2);
+let IMC2=(peso2/altura2**2);
+let IMC3=(peso3/altura3**2);
+let IMC4=(peso4/altura4**2);
+
+console.log(IMC1);
+console.log(IMC2);
+console.log(IMC3);
+console.log(IMC4);
+
+console.log(nome1 + sobrenome1 + " tem " + idade1 + " anos e seu índice de massa corporal é de ", IMC1 + " utiliza o plano", plano1);
+
